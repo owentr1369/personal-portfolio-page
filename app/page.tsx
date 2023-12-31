0d5e7fab5c11f1d006d2ee142bcf9dac3ec700b3
TranTam <tamvaa1306@gmail.com>
2023-12-31 20:49:55 +0700

    front-page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      Space Portfolio
+    <main className='h-full w-full'>
+      <div className='flex flex-col gap-20'>
+
+      </div>
     </main>
   )
 }
