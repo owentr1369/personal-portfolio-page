b85b32e4a68f7aec9de6499330b5c022c4c3fecb
TranTam <tamvaa1306@gmail.com>
2024-01-01 10:43:19 +0700

    Add animation and update content in HeroContent component

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
+import { slideInFromLeft, slideInFromTop } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/16/solid";
 
 const HeroContent = () => {
@@ -13,6 +13,20 @@ const HeroContent = () => {
                     <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
                     <h1 className="Welcome-text text-[13px]">Fullstack Developer Portfolio</h1>
                 </motion.div>
+                <motion.div
+                    variants={slideInFromLeft(0.5)}
+                    className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+
+                >
+                    Providing
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">the best</span>
+                    project experience
+                </motion.div>
+                <motion.p
+                    variants={slideInFromLeft(0.8)} className="text-lg text-gray-400 my-5 max-w-[600px]">
+                    I'm a Frontend Developer with nearly 2 years of experience in building web applications. I specialize in JavaScript and have professional experience working with React and Vue. Check out my projects and skills.
+                </motion.p>
+                <motion.a variants={slideInFromLeft(1)} className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">Learn More!</motion.a>
             </div>
         </motion.div>
     )
