dbe2b10aacddd676763193ce7a7e16548c46a6ee
TranTam <tamvaa1306@gmail.com>
2024-01-01 19:50:05 +0700

    add navbar logo

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2a0e61]/50 bg-[#3001417] backdrop-blur-md z-50 px-10 ">
-            Navbar
+            <div className="h-full w-full flex flex-row items-center justify-between m-auto px-[10px] ">
+                <a href="#aboutMe" className="h-auto w-auto flex flex-row items-center">
+                    <Image
+                        src='/NavLogo.png'
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="cursor-pointer hover:animate-slowsspin"
+                    />
+                    <span className="font-bold ml-[10px] hidden md:block text-gray-300">Owen Dev</span>
+                </a>
+            </div>
         </div>
     )
 }
