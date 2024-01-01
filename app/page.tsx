c40ec3fe14f9b9b3a64f033e0a2cc32c46377eb0
TranTam <tamvaa1306@gmail.com>
2024-01-01 19:59:24 +0700

    completed base

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
 import Hero from "../components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import Encryption from "@/components/main/Encryption";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
       </div>
     </main>
   );
