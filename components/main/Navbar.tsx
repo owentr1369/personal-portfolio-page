262eadd32b704425592ec62d2ba6b3f996591517
TranTam <tamvaa1306@gmail.com>
2024-01-01 19:43:52 +0700

    Add Navbar and update layout styles

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2a0e61]/50 bg-[#3001417] backdrop-blur-md z-50 px-10 ">
+            Navbar
+        </div>
+    )
+}
+
+
+export default Navbar;
