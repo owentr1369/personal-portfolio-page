4bfbdd9cd98d15f313c6b2fa0dd0aee50f03d060
Tâm Trần Thiện <tamtt1@firegroup.io>
2025-03-06 13:42:52 +0700

    feat: Integrate Hotjar for analytics tracking and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import Hero from "../components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 import Encryption from "@/components/main/Encryption";
-import '../assets/main.scss'
+import "../assets/main.scss";
+import Hotjar from "@hotjar/browser";
 
 export default function Home() {
+  const siteId = 5327937;
+  const hotjarVersion = 6;
+
+  Hotjar.init(siteId, hotjarVersion);
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
