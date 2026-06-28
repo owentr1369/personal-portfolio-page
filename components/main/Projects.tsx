0017c81cffa68a0aff75f3d4a362a046c8c8a6ff
Tâm Trần Thiện <tamvaa1306@gmail.com>
2026-06-28 17:53:09 +0700

    style: Adjust layout of Projects component for improved responsiveness and update SkillText component for consistent styling and content clarity

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
       <h1 className="text-2xl md:text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10 md:py-20">
         My Projects
       </h1>
-      <div className="h-full w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-10">
+      <div className="h-full w-full max-w-[1440px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-10">
         {ProjectsData.map((project, index) => (
           <ProjectCard
             key={index}
